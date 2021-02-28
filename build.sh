40992fd748bbdeb7cd6744f37d7d0987777484b8
David ALEXANDRE <david.alexandre@w6d.io>
2021-02-28 14:28:34 +0100

    add go mod vendor command for build

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ do
 name=$(basename $tool)
 echo "building $name"
 pushd $tool
+go mod vendor
 go build \
     -ldflags="-X 'main.Version=${VERSION}' -X 'main.Revision=${VCS_REF}' -X 'main.GoVersion=go${GOVERSION}' -X 'main.Built=${BUILD_DATE}' -X 'main.OsArch=${GOOS}/${GOARCH}'" \
     -mod=vendor \
